8ccf4eb5ad62e1dd53ac64a27adee47441e2a83e
Ramesh Avinash <ramesav@wnj10002lcvsmzl.home>
2017-04-19 19:38:06 -0400

    Initial Commit

---
+++ Weather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Weather
+//
+//  Created by Ramesh, Avinash on 4/19/17.
+//  Copyright © 2017 Ramesh, Avinash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Weather/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Weather
+//
+//  Created by Ramesh, Avinash on 4/19/17.
+//  Copyright © 2017 Ramesh, Avinash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
